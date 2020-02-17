1662ed1f81bde9e20f8cfafc16e8ac9c85a6dcf1
Najib <najib.muqeem@gmail.com>
2020-02-17 15:34:22 +0000

    added routes

--- express_server.js
+++ express_server.js
@@ -26,6 +26,14 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: req.params.longURL
+  };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
