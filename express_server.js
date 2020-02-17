0818137bc590ca69e5eec0be525cd4a2bfaaa222
Najib <najib.muqeem@gmail.com>
2020-02-17 16:10:15 +0000

    added functionality

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -18,12 +20,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -37,3 +48,21 @@ app.get("/urls/:shortURL", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  let str = "";
+  for (let i = 0; i < 6; i++) {
+    let digitCode = Math.floor(Math.random() * (57 - 48)) + 48;
+    let upCode = Math.floor(Math.random() * (90 - 65)) + 65;
+    let lowCode = Math.floor(Math.random() * (122 - 97)) + 97;
+    let digUpLow = Math.floor(Math.random() * 2);
+    if (digUpLow === 0) {
+      str += String.fromCharCode(digitCode);
+    } else if (digUpLow === 1) {
+      str += String.fromCharCode(upCode);
+    } else if (digUpLow === 2) {
+      str += String.fromCharCode(lowCode);
+    }
+  }
+  return str;
+}
