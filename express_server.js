ee5878169e749e2c90c17683f29653b8babf7b75
Najib <najib.muqeem@gmail.com>
2020-02-20 21:49:48 +0000

    little refactoring

--- express_server.js
+++ express_server.js
@@ -46,7 +46,11 @@ const urlsForUser = id => {
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.get("/hello", (req, res) => {
